649e77e361852e7acf6b450f048df207452e3420
Tricia Chow <suyeechow0212@gmail.com>
2023-05-11 15:28:15 +0800

    Change intro position

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -75,7 +75,7 @@ export default function Hero() {
 					</div>
 				</div>
 			</div>
-			<Intro />
+			{/* <Intro /> */}
 		</>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -77,6 +77,7 @@ export default function Home({ projects }) {
 					</div>
 				</div>
 			</section>
+			<Intro />
 			<Footer />
 		</>
 	);
