a49d5d11739da59c807d56452e0635a80edcc50a
Tricia Chow <suyeechow0212@gmail.com>
2023-05-18 18:54:38 +0800

    Update resume file

--- pages/resume.js
+++ pages/resume.js
@@ -8,8 +8,8 @@ export default function Resume() {
 		<>
 			<Navbar />
 			<section className="min-w-full lg:px-[140px] md:px-10 px-5 py-5 flex flex-col items-center mb-[400px]">
-				{/* <a
-					href="/Resume_Jan2023.pdf"
+				<a
+					href="/Resume_May_2023.pdf"
 					download
 					target="_blank"
 					rel="noopener noreferrer"
@@ -18,12 +18,12 @@ export default function Resume() {
 					<div className="rounded-2xl bg-[#0E7490]/50 border border-teal-400 hover:bg-[#164E63] p-8 flex justify-center items-center gap-5 font-medium lg:text-xl transition-all ease-in-out">
 						Download Tricia&apos;s resume here
 					</div>
-				</a> */}
-				<div className="h-full w-full lg:max-w-[1160px] flex justify-center">
+				</a>
+				{/* <div className="h-full w-full lg:max-w-[1160px] flex justify-center">
 					<div className="rounded-2xl bg-[#0E7490]/50 border border-teal-400 p-8 flex justify-center items-center gap-5 font-medium lg:text-xl transition-all ease-in-out">
 						Available to download soon
 					</div>
-				</div>
+				</div> */}
 			</section>
 			<Footer />
 		</>
